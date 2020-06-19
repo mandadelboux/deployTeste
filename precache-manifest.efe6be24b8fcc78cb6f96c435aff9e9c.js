7efc66eeb94ae5310b406773f73c81288b9fc915
Amanda <amandadelbouxvicente@hotmail.com>
2020-06-19 10:26:15 -0300

    Updates

---
+++ precache-manifest.efe6be24b8fcc78cb6f96c435aff9e9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f572da35b021e409831bc20cd2a5ebb",
+    "url": "/deployteste2/index.html"
+  },
+  {
+    "revision": "0fffd0c02cb918dbe169",
+    "url": "/deployteste2/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "55fb1d048c1dc4553172",
+    "url": "/deployteste2/static/css/main.6698de5b.chunk.css"
+  },
+  {
+    "revision": "0fffd0c02cb918dbe169",
+    "url": "/deployteste2/static/js/2.7337ee1c.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/deployteste2/static/js/2.7337ee1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fb1d048c1dc4553172",
+    "url": "/deployteste2/static/js/main.c4ea0027.chunk.js"
+  },
+  {
+    "revision": "67984c8d8021b4a4916b",
+    "url": "/deployteste2/static/js/runtime-main.6f7c6257.js"
+  },
+  {
+    "revision": "403ca5190471a9295a4789285b0dd884",
+    "url": "/deployteste2/static/media/logo-gcm.403ca519.png"
+  }
+]);
